455bbe3ff36fa111f61368c30d5e18934b733e54
Thaisleika <thaisleika@outlook.com>
2022-05-18 15:46:29 -0300

    l

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,21 +23,32 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
-
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
     return [];
   });
 
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const addNewProduct = addProduct(productId)
+      const sameProducts = updateProductAmount.map((product.id === productId) => )
+      const productAvailable = localStorage.getItem("@RocketShoes:cart")
+      if (sameProducts) {
+      }
+      if (productUnvailable)
+
+      
+      
     } catch {
-      // TODO
+      toast.error("Quantidade solicitada fora de estoque");
     }
+
+    localStorage.setCart('@RocketShoes:cart', cart)
+  
   };
 
   const removeProduct = (productId: number) => {
